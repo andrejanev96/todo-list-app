e136238a12029e375dc031380bf9251628a7c203
andrejanev96 <>
2025-05-29 20:56:45 +0200

    updated index.html, index.js and style.css files

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-import "./style.css";
+// src/index.js
+import "./style.css"; // Import our CSS file
+import { TodoApp } from "./modules/app.js";
+import { DOMManager } from "./modules/dom.js";
 
 console.log("Todo List App Starting...");
+
+// Initialize the application
+const app = new TodoApp();
+const domManager = new DOMManager(app);
+
+console.log("App initialized with projects:", app.projects);
+console.log("Current project:", app.getCurrentProject());
+
+// For debugging - expose app to window so we can test in console
+window.todoApp = app;
